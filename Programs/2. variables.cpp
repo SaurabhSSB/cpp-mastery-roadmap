169d66cb51c573efd9c6f34d1542cf288c49e0bc
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2025-01-20 17:47:38 +0530

    Adding variables file

---
+++ Programs/2. variables.cpp
@@ -0,0 +1,9 @@
+// -> used for comments part of program which is excluded during execution
+/*
+Multi-line comments
+*/
+
+// Variables are containers that store our data. 
+// Data type tell us how the computer should interpret the value of the data.
+// int float char double bool
+

0ed19a8ec8694f19ee16cf44c129ef160ee350c8
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2025-01-22 17:24:39 +0530

    Adding flow_control file

---
+++ Programs/11. flow_control.cpp
@@ -0,0 +1,14 @@
+// Flow Control
+// The order in which individual statements, instructions or function calls are executed and evaluated. 
+// Sequential_Execution Conditional_Statements Loops Break_and_Continue Switch/Case Exception_Handling
+
+// Conditional_Statments: if else if else
+// Loops: for While do-while
+// Break_and_Continue: Break Continue
+
+// Three Basic Control Structures:-      
+// Sequence Control Structure:- Entry- Action- Exit
+
+// Selection Control Structure:- Entry- Condition- Action- Exit
+
+// loop Control Structure:- Entry- loop- True- Action- Loop- False- Action- Exit

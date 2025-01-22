e98a690b91d2fcf123d3f12024b5cf4090b18885
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2025-01-22 17:25:42 +0530

    Adding while_loop file

---
+++ Programs/16. while.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+    // while(condition){
+    //     statement;
+    //     }
+
+    int i= 1;
+    while( i<= 40){
+        cout<< i<< endl;
+        i++;
+    }
+    return 0;
+}

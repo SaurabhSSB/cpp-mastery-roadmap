e3102befffc632573fbe8f57c477445ba8842081
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2025-01-22 17:24:28 +0530

    Adding operator_precedence file

---
+++ Programs/10. operator_precedence.cpp
@@ -0,0 +1,12 @@
+// en.cppreference.com
+// Operator Precedence which operator will be evaluated first.
+// Associativity way to solve operator with same precedence.
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    float a= 3+ 6* 27+ 11- 28/ 15;
+    cout<< a<< endl; 
+    return 0;
+}

65b7f0d6ff2f1b439f9a47a2f525f6758bf5d9b1
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2025-01-23 11:29:48 +0530

    Adding scope file

---
+++ Programs/3. scope.cpp
@@ -0,0 +1,19 @@
+// Scope is the region in code where the variable is valid. 
+// local and global scope
+// :: Scope resolution operator
+# include <iostream>
+using namespace std;
+
+int x= 45;
+void sum(){
+    cout<< x<< " ";
+}
+
+int main(){
+    int x= 27;
+    x= 101;
+    sum();
+    cout<< x<< " ";
+    cout<< ::x<<endl;
+    return 0;
+}

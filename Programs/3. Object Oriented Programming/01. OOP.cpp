bd65bb06574a0d5f1dae3a0c46516e6e7f9747f8
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2025-01-26 05:24:36 +0530

    Adding OOP file

---
+++ Programs/3. Object Oriented Programming/01. OOP.cpp
@@ -0,0 +1,11 @@
+// OOP consists of Class and Object
+// Class is a template for forming objects
+// OOP divides the problem into objects and builds data and functions around the object.
+
+// Classes- Basic template for creating objects.
+// Objects- Basic run time entities.
+// Data Abstraction & Encapsulation- Wrapping data and functions into single unit.
+// Inheritance- Properties of one class can be inherited into others.
+// Ploymorphism- Ability to take more than one forms.
+// Dynamic Binding- Code which will execute no known until the program execution.
+// Message Passing- Object.message(information) call format.

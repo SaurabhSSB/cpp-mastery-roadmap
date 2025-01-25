ededb716a8cf61ed5d9ac41c41fed514994d9d05
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2025-01-26 05:20:53 +0530

    Adding while file

---
+++ Programs/1. Procedural Oriented Programming/16. while.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // while(condition){
+    //     statement;
+    //     }
+
+    int i = 1;
+    while (i <= 40)
+    {
+        cout << i << endl;
+        i++;
+    }
+    return 0;
+}

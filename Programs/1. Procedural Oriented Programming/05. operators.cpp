a14d15b316cfd64f77b9196ecef1bc066216f01f
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2025-02-01 21:10:27 +0530

    Adding operators program file

---
+++ Programs/1. Procedural Oriented Programming/05. operators.cpp
@@ -0,0 +1,17 @@
+// Two types of header files
+// System header file already present in the compiler
+// User defined header file written by the programmer
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Type of operators
+    // Arithmetic Operators + _ * / % ++ --
+    // Assignment Operators =
+    // Comparison Operators == != >= <== > <
+    // Logical Operators && || !
+    // Bitwise Operators
+    return 0;
+}

040d68ffc8b2e2576bfb7f651321d690eb37f369
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2025-02-03 23:59:48 +0530

    Adding Friend_swap_pointer program file

---
+++ Programs/3. Object Oriented Programming/11.5. Friend_swap_pointer.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+class Origin_2;
+
+class Origin_1 {
+    int num_1;
+    friend void swap(Origin_1*, Origin_2*);
+public:
+    void set_num_1(int x) {
+        num_1 = x;
+    }
+    void get_num_1() {
+        cout << "The value of origin_1 is " << num_1 << endl;
+    }
+};
+
+class Origin_2 {
+    int num_2;
+    friend void swap(Origin_1*, Origin_2*);
+public:
+    void set_num_2(int y) {
+        num_2 = y;
+    }
+    void get_num_2() {
+        cout << "The value of origin_2 is " << num_2 << endl;
+    }
+};
+
+void swap(Origin_1* s, Origin_2* t) {
+    int temp = s->num_1;     // Access the private member using s->num_1
+    s->num_1 = t->num_2;     // Swap the values
+    t->num_2 = temp;
+    cout << "Successfully Swapped!" << endl;
+}
+
+int main() {
+    Origin_1 Original_1;
+    Original_1.set_num_1(25);
+    Original_1.get_num_1();
+    
+    Origin_2 Original_2;
+    Original_2.set_num_2(36);
+    Original_2.get_num_2();
+    
+    // Pass the addresses of the objects to the swap function
+    swap(&Original_1, &Original_2);
+    
+    Original_1.get_num_1();
+    Original_2.get_num_2();
+    
+    return 0;
+}

d26e582b62a1244008ed8eca04d7ddb4b4f73140
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2025-02-20 23:04:11 +0530

    Adding virtual_functions program file

---
+++ Programs/3. Object Oriented Programming/39. Virtual_functions.cpp
@@ -0,0 +1,35 @@
+# include<iostream>
+using namespace std;
+
+class B{
+    public:
+        int num_1;
+        void set_num_1(int a){
+            num_1= a;
+        }
+        virtual void get_num(){
+            cout<< "1. Value of number 1 is "<< num_1<< endl;
+        }
+};
+
+class D: public B{
+    public:
+        int num_2;
+        void set_num_2(int b){
+            num_2= b;
+        }
+        void get_num(){
+            cout<< "2. Value of number 1 is "<< num_1<< endl;
+            cout<< "2. Value of number 2 is "<< num_2<< endl;
+        }
+};
+
+int main(){
+    B *p_b;
+    B o_b;
+    D o_d;
+    p_b= &o_d; 
+    p_b-> num_1= 27;
+    p_b-> get_num();
+    return 0;
+}
